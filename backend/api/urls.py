0da4051add664579e2eeb7f1709004ed187d2e8c
Malianurag07 <malianuraghr07@gmail.com>
2025-12-24 00:05:48 +0530

    Fix URLs and upload database/images

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,27 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include
-
-# We do NOT import 'views' here.
-
-# Import these two for media files
+from django.urls import path, include, re_path  # <-- Added re_path here
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve           # <-- Added serve here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # This line tells Django to look at your 'cart/urls.py' file
+    # Cart URLs
     path('cart/', include('cart.urls', namespace='cart')),
 
-    path('customizer/', include('customizer.urls')), # <--- Add this
+    # Customizer URLs
+    path('customizer/', include('customizer.urls')),
     
-    # This line tells Django to look at your 'products/urls.py' file
+    # Wishlist URLs
+    path('wishlist/', include('wishlist.urls')),
+
+    # Product URLs (Homepage)
     path('', include('products.urls', namespace='products')),
-    
-    path('wishlist/', include('wishlist.urls')),  # ✅ add this lin
 ]
 
-# This is also necessary to see your shoe images
+# Standard Local Development Static Files
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# 👇 "RESUME MODE" - Force serve static/media files on Render 👇
+# This manually handles images even when DEBUG=False on the live server
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
